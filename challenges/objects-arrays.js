b66dbb68b882031212ec3f47320d9e624ae34794
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-14 09:10:22 -0600

    finished tyrannosaurus object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,6 +8,14 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  lenght: '12m',
+  period: 'Late Cretaceous'
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
